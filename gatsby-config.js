df7400e66cbbe9c3480bdc1d2d74e39a14b28e0b
sopnep15 <sophia.nepia@icloud.com>
2019-06-21 19:40:38 +1200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,6 +128,21 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-W6752SW",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // Specify optional GTM environment details.
+        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+        dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
     "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-feed",
